4d5453d5a179eab7ffec3f7e4ee8c085dd44c106
pradnya-16 <74720164+pradnya-16@users.noreply.github.com>
2021-08-13 22:22:10 +0530

    Add files via upload

---
+++ ListInRange.py
@@ -0,0 +1,21 @@
+# condition for the first list
+list1 =  [12, -7, 5, 64, -14]
+for i in list1:
+    if i>=0:
+       print(i, end = " ")
+       
+
+# condition for the second list
+list2 = [12, 14, -95, 3]
+i=0
+
+while(len(list2) > i):
+    if list2[i] >= 0:
+        print (list2[i], end = " ")
+
+    i=i+1
+
+	
+
+
+	
