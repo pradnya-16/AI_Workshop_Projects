fdec54e31773da895bb15b2d5e20db6b06fce261
pradnya-16 <74720164+pradnya-16@users.noreply.github.com>
2021-08-26 19:39:47 +0530

    Add files via upload
    This is the code for the set operations in python.

---
+++ setoperations.py
@@ -0,0 +1,15 @@
+#Initializing the sets
+E = {2,4,8,0,6}
+N = {1,2,3,4,5}
+
+#Union of sets
+print("Union of E and N is:\n",E|N)
+
+#Intersection of sets
+print("Intersection of E and N is:\n",E&N)
+
+#Difference of two sets
+print("Difference of E and N is:\n",E-N)
+
+#Symmentric difference of two sets
+print("Symmetric difference of E and N is:\n",E^N)
